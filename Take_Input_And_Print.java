dcb9d4246e6801568feae555e75b389b68f56eeb
22P31A0445 <119718890+22P31A0445@users.noreply.github.com>
2023-05-16 23:12:56 +0530

    import java.util.*; //import java.lang.*; public class Sirisha {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String c=sc.nextLine();         System.out.println(c);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+//import java.lang.*;
+public class Sirisha
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String c=sc.nextLine();
+        System.out.println(c);
+    }
+}
