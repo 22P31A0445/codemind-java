fa0bb585ed4466a1c5045ddecd8531f2c2def69d
22P31A0445 <119718890+22P31A0445@users.noreply.github.com>
2023-08-06 19:18:43 +0530

    import java.util.Scanner; public class ASCII {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         char a;         a=sc.nextLine().charAt(0);         System.out.println((int)a);     } }

--- Find_ASCII_Value_of_a_Character.java
+++ Find_ASCII_Value_of_a_Character.java
@@ -1,10 +1,11 @@
-import java.util.*;
-public class sol
+import java.util.Scanner;
+public class ASCII
 {
-    public static void main(String args[])
+    public static void main(String[] args)
     {
-        Scanner sc=new Scanner(System.in);
-        char ch=sc.nextLine().charAt(0);
-        System.out.println((int)ch);
+        Scanner sc = new Scanner(System.in);
+        char a;
+        a=sc.nextLine().charAt(0);
+        System.out.println((int)a);
     }
 }
